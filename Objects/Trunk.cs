c4664a7d7433c22fe7954763b04c7c1a75cf6697
john dowd and nick wise <john+nick@>
2017-06-05 15:39:37 -0700

    Add all methods and test methods.

--- Objects/Trunk.cs
+++ Objects/Trunk.cs
@@ -16,6 +16,20 @@ namespace Trunk
     _name = name;
     }
 
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+        Item newItem = (Item) otherItem;
+        bool idEquality = (this.GetId() == newItem.GetId());
+        bool nameEquality = (this.GetName() == newItem.GetName());
+        return (idEquality && nameEquality);
+      }
+    }
 
     public int GetId()
     {
@@ -26,19 +40,19 @@ namespace Trunk
     {
       return _name;
     }
-    public string SetName(string name)
+    public void SetName(string name)
     {
       _name = name;
     }
 
-    public static List<Item> GetALL()
+    public static List<Item> GetAll()
     {
       List<Item> allItems = new List<Item>{};
 
       SqlConnection conn = DB.Connection();
       conn.Open();
 
-      SqlCommand cmd = new DB.SqlCommand("SELECT * from items;", conn);
+      SqlCommand cmd = new SqlCommand("SELECT * from items;", conn);
       SqlDataReader rdr = cmd.ExecuteReader();
 
       while(rdr.Read())
@@ -59,6 +73,72 @@ namespace Trunk
       }
       return allItems;
     }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
 
+      SqlCommand cmd = new SqlCommand("INSERT INTO items (name) OUTPUT INSERTED.id VALUES (@ItemName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@ItemName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM items;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+
+    public static Item Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd =new SqlCommand("SELECT * FROM items WHERE id = @ItemId;", conn);
+      SqlParameter itemIdParameter = new SqlParameter();
+      itemIdParameter.ParameterName = "@ItemId";
+      itemIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(itemIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundItemId = 0;
+      string foundItemName = null;
+      while(rdr.Read())
+      {
+        foundItemId = rdr.GetInt32(0);
+        foundItemName = rdr.GetString(1);
+      }
+      Item foundItem = new Item(foundItemName, foundItemId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundItem;
+    }
   }
 }
--- Tests/TrunkTests.cs
+++ Tests/TrunkTests.cs
@@ -8,7 +8,7 @@ namespace Trunk
 {
   public class TrunkTest : IDisposable
   {
-    public TrunkTest
+    public TrunkTest()
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=trunk_test;Integrated Security=SSPI;";
     }
@@ -17,11 +17,59 @@ namespace Trunk
     public void Test_DatabaseEmptyAtFirst()
     {
       //Ararange , Act
-      int result = Item.GetALL().Count;
+      int result = Item.GetAll().Count;
 
       Assert.Equal(0,result);
     }
 
+    [Fact]
+    public void Test_Equal_ReturnsTrueIfNamesAreSame()
+    {
+      Item firstItem = new Item("Concert Tickets");
+      Item secondItem = new Item("Concert Tickets");
+
+      Assert.Equal(firstItem, secondItem);
+    }
+
+    [Fact]
+    public void Test_Save_SavesToDatabase()
+    {
+      //Arrange
+      Item testItem = new Item("Concert Tickets");
+
+      //Act
+      testItem.Save();
+      List<Item> result = Item.GetAll();
+      List<Item> testList = new List<Item>{testItem};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_Save_AssignsIdToObject()
+    {
+      Item testItem = new Item("Concert Tickets");
+
+      testItem.Save();
+      Item savedItem = Item.GetAll()[0];
+
+      int testId = testItem.GetId();
+      int result = savedItem.GetId();
+
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Test_Find_FindsItemsInDatabase()
+    {
+      Item testItem = new Item ("Concert Tickets");
+      testItem.Save();
+
+      Item foundItem = Item.Find(testItem.GetId());
+
+      Assert.Equal(testItem, foundItem);
+    }
     public void Dispose()
     {
       Item.DeleteAll();
